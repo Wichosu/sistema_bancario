c2c44778e6b7eae2528f1dc186b2a5e57d1bc282
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-07 14:44:40 -0600

    Added Link to actividad.tsx

--- app/operaciones/actividad/page.tsx
+++ app/operaciones/actividad/page.tsx
@@ -1,5 +1,6 @@
 import Movement from "@/components/Movement";
 import Subtitle from "@/components/Subtitle";
+import Link from "next/link";
 
 const movements = [
   {
@@ -23,6 +24,14 @@ export default function Page(){
           />
         ))
       }
+      <div className="mt-8">
+        <Link 
+          href={'/'} 
+          className="bg-neutral-300 px-8 py-2 rounded"
+        >
+          Regresar
+        </Link>
+      </div>
     </>
   )
 }
