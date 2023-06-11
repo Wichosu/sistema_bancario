521c1b632704d217bb29a72bb10b68da9578f39f
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-11 13:08:44 -0600

    Example array of coins added

---
+++ lib/coins.ts
@@ -0,0 +1,35 @@
+//1MXN = value of coin
+export const coins = [
+  {
+    name: "Dolar Americano",
+    value: 0.058
+  },
+  {
+    name: "Peso Colombiano",
+    value: 241.36
+  },
+  {
+    name: "Peso Chileno",
+    value: 45.56
+  },
+  {
+    name: "Sol Peruano",
+    value: 0.21
+  },
+  {
+    name: "Peso Argentino",
+    value: 14.13
+  },
+  {
+    name: "Yuan",
+    value: 0.41
+  },
+  {
+    name: "Yen",
+    value: 8.06
+  },
+  {
+    name: "Euro",
+    value: 0.054
+  }
+]
