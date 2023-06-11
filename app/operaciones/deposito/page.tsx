0b016dc0d45a440d1425b58dfaeee3ab119c0417
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-10 21:39:51 -0600

    Deposit now working, still bare bones

--- app/operaciones/deposito/page.tsx
+++ app/operaciones/deposito/page.tsx
@@ -1,12 +1,36 @@
 import Footer from "@/components/Footer";
 import Input from "@/components/Input";
 import Subtitle from "@/components/Subtitle";
+import clientPromise from "@/lib/mongodb";
+import { redirect } from "next/navigation";
+
+async function handleForm(data : FormData) {
+  "use server"
+  const client = await clientPromise
+  const account = data.get('num_cuenta')?.valueOf()
+  const amount = data.get('cantidad')?.valueOf()
+  
+  
+  if(typeof account !== "string" || typeof amount !== "string") {
+    throw new Error("Wrong type")
+  }
+  
+  
+  const prev = await client.db("Banco").collection("Cuenta").findOne({ numero_cuenta: account })
+
+  client.db("Banco").collection("Cuenta").updateOne(
+    { numero_cuenta: account},
+    { $set: { fondos: (+amount + prev?.fondos) }}
+  )
+
+  redirect('/')
+}
 
 export default function Page() {
   return(
     <>
       <Subtitle subtitle="Deposito" />
-      <form className="flex flex-col gap-8">
+      <form action={handleForm} method="POST" className="flex flex-col gap-8">
         <Input label="Num. Cuenta" name="num_cuenta" />
         <div className="flex gap-8">
           <Input label="Nombre" name="nombre" />
