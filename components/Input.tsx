a9c9774e6585abaa13295bbf16ba231536cd5de9
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-14 22:18:43 -0600

    Input component with more options

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,20 +1,37 @@
 interface Props {
   label: string,
   type?: string,
-  name: string
+  name?: string,
+  value?: string,
+  disabled?: boolean
 }
 
-export default function Input({ label, type = 'text', name} : Props) {
+export default function Input({ label, type = 'text', name, value, disabled} : Props) {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={name} >{ label }</label>
-      <input 
-        id={name} 
-        name={name} 
-        type={type} 
-        className="border border-neutral-500 w-fit rounded-sm"
-        required
-      />
+      {
+        disabled
+        ?
+          <input 
+            id={name} 
+            name={name} 
+            type={type} 
+            value={value}
+            disabled
+            className="border border-neutral-500 w-fit rounded-sm"
+            required
+          />
+        :
+          <input 
+            id={name} 
+            name={name} 
+            type={type} 
+            value={value}
+            className="border border-neutral-500 w-fit rounded-sm"
+            required
+          />
+      }
     </div>
   )
 }
