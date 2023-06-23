cf2ad7ee6f3a83e22cc2ae0bf2ac726526f53240
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-22 20:02:13 -0600

    Added createCoinExchangeMovements server component

--- app/operaciones/divisas/compra/page.tsx
+++ app/operaciones/divisas/compra/page.tsx
@@ -1,25 +1,8 @@
 import Subtitle from "@/components/Subtitle";
-import clientPromise from "@/lib/mongodb";
-import { MovementCoinExchange, CoinExchangeModes } from "@/types";
+import { CoinExchangeModes } from "@/types";
 import CoinExchange from "@/components/CoinExchange";
 import { isAuth } from "@/lib/isAuth";
-import { getWindowNumber } from "@/lib/getWindowNumber";
-
-async function createMovement(movement: MovementCoinExchange) {
-  "use server"
-  try {
-    const client = await clientPromise
-
-    const movement_w_window: MovementCoinExchange = {
-      ...movement,
-      ventanilla: getWindowNumber()
-    }
-
-    client.db("Banco").collection("Movimiento_Divisa").insertOne(movement_w_window)
-  } catch(e) {
-    console.log(e)
-  }
-}
+import { createMovement } from "@/server/createCoinExchangeMovement";
 
 export default function Page(){
   isAuth()
--- app/operaciones/divisas/venta/page.tsx
+++ app/operaciones/divisas/venta/page.tsx
@@ -1,25 +1,10 @@
 import Subtitle from "@/components/Subtitle";
-import clientPromise from "@/lib/mongodb";
-import { MovementCoinExchange, CoinExchangeModes } from "@/types";
+import { CoinExchangeModes } from "@/types";
 import CoinExchange from "@/components/CoinExchange";
 import { isAuth } from "@/lib/isAuth";
-import { getWindowNumber } from "@/lib/getWindowNumber";
+import { createMovement } from "@/server/createCoinExchangeMovement";
 
-async function createMovement(movement: MovementCoinExchange) {
-  "use server"
-  try {
-    const client = await clientPromise
 
-    const movement_w_window: MovementCoinExchange = {
-      ...movement,
-      ventanilla: getWindowNumber()
-    }
-
-    client.db("Banco").collection("Movimiento_Divisa").insertOne(movement_w_window)
-  } catch(e) {
-    console.log(e)
-  }
-}
 
 export default function Page(){
   isAuth()
