7d4aa237ac3f4d9c9f3110af961f39ac2df45888
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-22 20:19:09 -0600

    Added placeholder to SearchAccount inputs

--- components/SearchAccount.tsx
+++ components/SearchAccount.tsx
@@ -35,6 +35,7 @@ export default function SearchAccount({ getBalance } : Props) {
           pattern="\d{2}"
           className="border border-neutral-500 rounded focus:outline-none focus:border-blue-500
           focus:invalid:border-red-500 invalid:border-red-500"
+          placeholder="00"
           onChange={handleInput}
           ref={firstInput}
         />
@@ -46,6 +47,7 @@ export default function SearchAccount({ getBalance } : Props) {
           pattern="\d{4}"
           className="border border-neutral-500 rounded focus:outline-none focus:border-blue-500
           focus:invalid:border-red-500 invalid:border-red-500"
+          placeholder="0000"
           onChange={handleInput}
           ref={secondInput}
         />
