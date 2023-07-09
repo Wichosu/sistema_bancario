fa37018a95b428d0319c214fcbf409052921e9cb
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-07-08 21:24:05 -0600

    Added isAuth() to asignar-empleado route

--- app/admin/asignar-empleado/page.tsx
+++ app/admin/asignar-empleado/page.tsx
@@ -1,7 +1,10 @@
 import Subtitle from "@/components/Subtitle";
 import AssignEmploye from "@/components/AssignEmploye";
+import { isAuth } from "@/lib/isAuth";
 
 export default function Page() {
+  isAuth()
+
   return (
     <>
       <Subtitle subtitle="Asignar Empleado" />
